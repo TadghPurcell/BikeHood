c4ae0edfe1462b618bb7dbfa3dbe3f38b8f41ac5
DR7439 <146729680+DR7439@users.noreply.github.com>
2025-03-03 12:28:52 +0000

    fixed landing page error

--- client/src/pages/LandingPage.tsx
+++ client/src/pages/LandingPage.tsx
@@ -1,7 +1,6 @@
 import Header from "../components/Header";
 import Hero from "../components/Hero";
 import Info from "../components/Info";
-import Twin from "../components/Twin";
 import Contact from "../components/Contact";
 import Footer from "../components/Footer";
 import Analytics from "../components/Analytics";
@@ -13,7 +12,6 @@ const LandingPage: React.FC = () => {
       <main className="font-nunito">
         <Hero />
         <Info />
-        <Twin />
         <Analytics />
         <Contact />
       </main>
